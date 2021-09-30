db514500bb174952acb70494598dce940e29fb89
jinlafan <msk57jinlafan@yahoo.co.jp>
2021-09-30 18:46:58 +0900

    add dropnet mechanism

--- nmtpytorch/layers/attention/hierarchical.py
+++ nmtpytorch/layers/attention/hierarchical.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 import torch
 from torch import nn
 
@@ -13,7 +14,8 @@ from ...utils.nn import get_activation_fn
 
 class HierarchicalAttention(nn.Module):
     """Hierarchical attention over multiple modalities."""
-    def __init__(self, ctx_dims, hid_dim, mid_dim, att_activ='tanh'):
+    def __init__(self, ctx_dims, hid_dim, mid_dim, att_activ='tanh', dropnet=False, \
+    dropnet_image_rate=0.15, dropnet_text_rate=0.15):
         super().__init__()
 
         self.activ = get_activation_fn(att_activ)
@@ -21,12 +23,18 @@ class HierarchicalAttention(nn.Module):
         self.hid_dim = hid_dim
         self.mid_dim = mid_dim
 
+        self.dropnet = dropnet
+        self.dropnet_image_rate = dropnet_image_rate
+        self.dropnet_text_rate = dropnet_text_rate
+
         self.ctx_projs = nn.ModuleList([
             nn.Linear(dim, mid_dim, bias=False) for dim in self.ctx_dims])
         self.dec_proj = nn.Linear(hid_dim, mid_dim, bias=True)
         self.mlp = nn.Linear(self.mid_dim, 1, bias=False)
 
     def forward(self, contexts, hid):
+        # contexts[0] --> textual context
+        # contexts[1] --> visual context
         dec_state_proj = self.dec_proj(hid)
         ctx_projected = torch.cat([
             p(ctx).unsqueeze(0) for p, ctx
@@ -35,6 +43,24 @@ class HierarchicalAttention(nn.Module):
         att_dist = nn.functional.softmax(energies, dim=0)
 
         ctxs_cat = torch.cat([c.unsqueeze(0) for c in contexts])
-        joint_context = (att_dist * ctxs_cat).sum(0)
+        
+        # dropnet mechanism
+        if self.training == True:
+            if self.dropnet == True:
+                x = random.choices([0, 1, 2], k=1, weights=[self.dropnet_image_rate, \
+                self.dropnet_text_rate, 1-self.dropnet_image_rate-self.dropnet_text_rate])[0]
+                # use only textual context
+                if x == 0:
+                    joint_context = (att_dist * ctxs_cat)[0]
+                # use only image context
+                elif x == 1:
+                    joint_context = (att_dist * ctxs_cat)[1]
+                # use both visual and textual contexts (i.e. multimodal context)
+                else:
+                    joint_context = (att_dist * ctxs_cat).sum(0)
+            else: 
+                joint_context = (att_dist * ctxs_cat).sum(0)
+        else: 
+            joint_context = (att_dist * ctxs_cat).sum(0)
 
         return att_dist, joint_context
